8b670dd223f9a77db25c33b7098c282a7c552ad8
zs <zs@bjpowernode.com>
2020-11-30 15:22:38 +0800

    Initial commit

---
+++ src/main/java/com/emtor/SpringbootGit1Application.java
@@ -0,0 +1,13 @@
+package com.emtor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGit1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGit1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/emtor/SpringbootGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGit1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
